350994a7b5b6525689fc9b70fd461a37f31adce0
David Robillard <d@drobilla.net>
2022-02-02 22:53:23 -0500

    Fix MinGW build

--- src/warnings.h
+++ src/warnings.h
@@ -13,7 +13,7 @@
 
 #  define EXESS_RESTORE_WARNINGS _Pragma("clang diagnostic pop")
 
-#elif defined(__GNUC__) && __GNUC__ >= 10
+#elif defined(__GNUC__) && __GNUC__ >= 8
 
 #  define EXESS_DISABLE_CONVERSION_WARNINGS                  \
     _Pragma("GCC diagnostic push")                           \
