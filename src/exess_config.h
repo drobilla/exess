134cc74024d212ef219458b08494f5f3789b5f41
David Robillard <d@drobilla.net>
2023-02-22 13:25:35 -0500

    Improve system feature detection

--- src/exess_config.h
+++ src/exess_config.h
@@ -1,15 +1,35 @@
-// Copyright 2021 David Robillard <d@drobilla.net>
+// Copyright 2021-2023 David Robillard <d@drobilla.net>
 // SPDX-License-Identifier: ISC
 
 /*
-  Configuration header that defines reasonable defaults at compile time.
+  Configuration header that defines reasonable defaults at compile-time.
 
-  This allows compile-time configuration from the command line (typically via
-  the build system) while still allowing the source to be built without any
-  configuration.  The build system can define EXESS_NO_DEFAULT_CONFIG to disable
-  defaults, in which case it must define things like HAVE_FEATURE to enable
-  features.  The design here ensures that compiler warnings or
-  include-what-you-use will catch any mistakes.
+  This allows configuration from the command-line (usually by the build system)
+  while still allowing the code to compile "as-is" with reasonable default
+  features on supported platforms.
+
+  This system is designed so that, ideally, no command-line or build-system
+  configuration is needed, but automatic feature detection can be disabled or
+  overridden for maximum control.  It should never be necessary to edit the
+  source code to achieve a given configuration.
+
+  Usage:
+
+  - By default, features are enabled if they can be detected or assumed to be
+    available from the build environment, unless `EXESS_NO_DEFAULT_CONFIG` is
+    defined, which disables everything by default.
+
+  - If a symbol like `HAVE_SOMETHING` is defined to non-zero, then the
+    "something" feature is assumed to be available.
+
+  Code rules:
+
+  - To check for a feature, this header must be included, and the symbol
+    `USE_SOMETHING` used as a boolean in an `#if` expression.
+
+  - None of the other configuration symbols described here may be used
+    directly.  In particular, this header should be the only place in the
+    source code that touches `HAVE` symbols.
 */
 
 #ifndef EXESS_CONFIG_H
@@ -21,32 +41,24 @@
 #if !defined(EXESS_NO_DEFAULT_CONFIG)
 
 // GCC and clang: __builtin_clz()
-#  ifndef HAVE_BUILTIN_CLZ
+#  if !defined(HAVE_BUILTIN_CLZ)
 #    if defined(__has_builtin)
 #      if __has_builtin(__builtin_clz)
 #        define HAVE_BUILTIN_CLZ 1
-#      else
-#        define HAVE_BUILTIN_CLZ 0
 #      endif
 #    elif defined(__GNUC__)
 #      define HAVE_BUILTIN_CLZ 1
-#    else
-#      define HAVE_BUILTIN_CLZ 0
 #    endif
 #  endif
 
 // GCC and clang: __builtin_clz()
-#  ifndef HAVE_BUILTIN_CLZLL
+#  if !defined(HAVE_BUILTIN_CLZLL)
 #    if defined(__has_builtin)
 #      if __has_builtin(__builtin_clzll)
 #        define HAVE_BUILTIN_CLZLL 1
-#      else
-#        define HAVE_BUILTIN_CLZLL 0
 #      endif
 #    elif defined(__GNUC__)
 #      define HAVE_BUILTIN_CLZLL 1
-#    else
-#      define HAVE_BUILTIN_CLZLL 0
 #    endif
 #  endif
 
@@ -60,7 +72,16 @@
   if the build system defines them all.
 */
 
-#define USE_BUILTIN_CLZ HAVE_BUILTIN_CLZ
-#define USE_BUILTIN_CLZLL HAVE_BUILTIN_CLZLL
+#if defined(HAVE_BUILTIN_CLZ) && HAVE_BUILTIN_CLZ
+#  define USE_BUILTIN_CLZ 1
+#else
+#  define USE_BUILTIN_CLZ 0
+#endif
+
+#if defined(HAVE_BUILTIN_CLZLL) && HAVE_BUILTIN_CLZLL
+#  define USE_BUILTIN_CLZLL 1
+#else
+#  define USE_BUILTIN_CLZLL 0
+#endif
 
 #endif // EXESS_CONFIG_H
