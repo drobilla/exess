f289b55d05eefc3489061a9bb36f1f82c30f969f
David Robillard <d@drobilla.net>
2024-06-04 22:47:48 -0400

    Fix whitespace

--- src/read_utils.h
+++ src/read_utils.h
@@ -10,12 +10,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
-EXESS_PURE_FUNC
-size_t
+EXESS_PURE_FUNC size_t
 skip_whitespace(const char* str);
 
-EXESS_CONST_FUNC
-bool
+EXESS_CONST_FUNC bool
 is_end(char c);
 
 ExessResult
@@ -24,8 +22,7 @@ read_two_digit_number(uint8_t*    out,
                       uint8_t     max_value,
                       const char* str);
 
-EXESS_PURE_FUNC
-ExessResult
+EXESS_PURE_FUNC ExessResult
 end_read(ExessStatus status, const char* str, size_t i);
 
 #endif // EXESS_SRC_READ_UTILS_H
