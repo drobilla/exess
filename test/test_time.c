f85cd7b720d0e2c37a254bf95d41d40c99bd8169
David Robillard <d@drobilla.net>
2025-08-19 16:14:46 -0400

    Avoid clang-format instability

--- test/test_time.c
+++ test/test_time.c
@@ -195,11 +195,11 @@ test_write_time(void)
 static void
 check_round_trip(const ExessTime value)
 {
-  ExessTime parsed_value                   = {EXESS_LOCAL, 0, 0, 0, 0};
-  char      buf[EXESS_MAX_TIME_LENGTH + 1] = {1,  2,  3,  4,  5,  6,  7,  8,  9,
-                                              10, 11, 12, 13, 14, 15, 16, 17, 18,
-                                              19, 20, 21, 22, 23, 24, 25};
+  char buf[EXESS_MAX_TIME_LENGTH + 1] = {1,  2,  3,  4,  5,  6,  7,  8,  9,
+                                         10, 11, 12, 13, 14, 15, 16, 17, 18,
+                                         19, 20, 21, 22, 23, 24, 25};
 
+  ExessTime parsed_value = {EXESS_LOCAL, 0, 0, 0, 0};
   assert(!exess_write_time(value, sizeof(buf), buf).status);
   assert(!exess_read_time(&parsed_value, buf).status);
   assert(parsed_value.hour == value.hour);
--- test/test_timezone.c
+++ test/test_timezone.c
@@ -169,12 +169,12 @@ test_write_timezone(void)
 static void
 check_round_trip(const ExessTimezone value)
 {
-  ExessTime parsed_time                    = {EXESS_LOCAL, 0, 0, 0, 0};
-  char      buf[EXESS_MAX_TIME_LENGTH + 1] = {1,  2,  3,  4,  5,  6,  7,  8,  9,
-                                              10, 11, 12, 13, 14, 15, 16, 17, 18,
-                                              19, 20, 21, 22, 23, 24, 25};
+  char buf[EXESS_MAX_TIME_LENGTH + 1] = {1,  2,  3,  4,  5,  6,  7,  8,  9,
+                                         10, 11, 12, 13, 14, 15, 16, 17, 18,
+                                         19, 20, 21, 22, 23, 24, 25};
 
-  const ExessTime time = {value, 12, 0, 0, 0};
+  const ExessTime time        = {value, 12, 0, 0, 0};
+  ExessTime       parsed_time = {EXESS_LOCAL, 0, 0, 0, 0};
 
   assert(!exess_write_time(time, sizeof(buf), buf).status);
   assert(!exess_read_time(&parsed_time, buf).status);
